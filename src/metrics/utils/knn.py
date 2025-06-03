5b180a4e1ce0290568acdf54b3d1d57edeafb9f6
davidht02 <david.hidalgo.trabazo@estudiantat.upc.edu>
2025-06-03 20:53:47 +0200

    Update knn.py

--- src/metrics/utils/knn.py
+++ src/metrics/utils/knn.py
@@ -1,12 +1,12 @@
 import numpy as np
-from sklearn.neighbors import KDTree
+from sklearn.neighbors import KDTree, BallTree
 
 class KNNFinder:
     """
     Class to find the k-nearest neighbors using KDTree.
     """
 
-    def __init__(self, data: np.ndarray, leaf_size: int = 40):
+    def __init__(self, data: np.ndarray, leaf_size: int = 40, tree_type: str = 'kd_tree'):
         """
         Initializes the KDTree with the provided data.
 
@@ -18,7 +18,13 @@ class KNNFinder:
             Number of points in the leaf nodes. Default is 40.
         """
         self.data = data
-        self.tree = KDTree(data, leaf_size=leaf_size)
+        self.tree_type = tree_type.lower()
+        if self.tree_type == 'kd_tree':
+            self.tree = KDTree(data, leaf_size=leaf_size)
+        elif self.tree_type == 'ball_tree':
+            self.tree = BallTree(data, leaf_size=leaf_size)
+        else:
+            raise ValueError("tree_type debe ser 'kd_tree' o 'ball_tree'")
 
     def query(self, points: np.ndarray, k: int = 5) -> np.ndarray:
         """
